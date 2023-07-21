1d6c7765f0ccaa49eca9427c4380d82417ff3535
Marcos Hernandez <marcos.hernandez.dev@gmail.com>
2023-07-21 00:47:49 -0300

    feat: views inicio, registrarse y tareas

--- tareas/views.py
+++ tareas/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.http import HttpResponse
+from django.db import IntegrityError
 
 # Create your views here.
+def inicio(request):
+	return render(request, 'inicio.html')
+
+def registrarse(request):
+	if request.method == 'GET':
+		return render(request, 'registrarse.html', {
+			'form': UserCreationForm
+		})
+	else:
+		if request.POST['password1'] == request.POST['password2']:
+			try:
+				usuario = User.objects.create_user(username=request.POST['username'], password = request.POST['password1'])
+				usuario.save()
+				login(request, usuario)
+				return redirect('tareas')
+			except IntegrityError:
+				return render(request, 'registrarse.html', {
+					'form': UserCreationForm,
+					'error': 'El usuario ya existe'
+				})
+		return render(request, 'registrarse.html', {
+			'form': UserCreationForm,
+			'error': 'La contraseña no coincide'
+			})
+
+def tareas(request):
+	return render(request, 'tareas.html')
